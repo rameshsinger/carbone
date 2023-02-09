ac5e215b58188bf74076d5314aef2a2b700974c9
ramesh singer <rameshsinger93@gmail.com>
2023-02-10 02:02:22 +0530

    Total Page Update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs');
 const carbone = require('./lib/index');
+var docxXMLEditor = require('./lib/DocxXMLEditor');
 const app = express()
 const port = 3000
 app.use(express.json({limit: '150mb'}));
@@ -14,7 +15,7 @@ app.post('/report', function (req, res) {
       let templatePath=req.body.templatePath;
       let destinationPath=req.body.destinationPath;
       let reportData=req.body.reportData;
-      carbone.render(templatePath, reportData, options, function(err,result ){
+      carbone.render(templatePath, reportData, options, async function(err,result ){
         if (err) {
           console.log(err);
             res.send('Failure')
@@ -22,6 +23,7 @@ app.post('/report', function (req, res) {
         }
         fs.writeFileSync(destinationPath, result);
         console.log("File Generated successfully:"+destinationPath)
+        await docxXMLEditor.updateTotalPageNumber(destinationPath,1);
         res.send('SUCCESS')
         // process.exit(); // to kill automatically LibreOffice workers
       });
